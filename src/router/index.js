a7a1f9ac9921ad15f924e7fe86ec8ace6e399836
Antonio Montilla <system.montilla@gmail.com>
2024-07-04 15:14:15 +0800

    Vue JS Job Posting

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from '@/views/HomeView.vue';
+import Jobsview from '@/views/Jobsview.vue';
+import NotFoundView from '@/views/NotFoundView.vue';
+import JobView from '@/views/JobView.vue';
+import AddJobView from '@/views/AddJobView.vue';
+import EditJobView from '@/views/EditJobView.vue';
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView,
+        },
+        {
+          path: '/jobs',
+          name: 'jobs',
+          component: Jobsview,
+        },
+        {
+          path: '/job/:id',
+          name: 'view-job',
+          component: JobView,
+        },
+        {
+          path: '/jobs/add',
+          name: 'add-job',
+          component: AddJobView,
+        },
+        {
+          path: '/jobs/edit/:id',
+          name: 'edit-job',
+          component: EditJobView,
+        },
+        {
+          path: '/:catchAll(.*)',
+          name: 'not-found',
+          component: NotFoundView,
+        }
+      ]
+  });
+
+export default router;
